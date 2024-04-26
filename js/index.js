4cc578ca5e8cf66c6bf13e3ce293e8bc53c147af
HiYUNo <jayhyun1008@gmail.com>
2024-04-27 08:45:36 +0900

    廢get之法而加input, textarea之

--- js/index.js
+++ js/index.js
@@ -15,19 +15,28 @@ function getQueryStringObject() {
 
 var qs = getQueryStringObject()
 
+
 var base64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
 var smalltextarr = []
 var texttonumarr = []
-var text = ''
-var title = qs.title.replace(/\\n/g, '<br></br>')
+if (!document.querySelector('#book-content').value) {
+    document.querySelector('#book-content').value = "子曰學而時習之不亦說乎\n'學之爲言效也人性皆善而覺有先後後覺者必效先覺之所爲乃可以明善而復其初也習鳥數飛也學之不已如鳥數飛也說喜意也旣學而又時時習之則所學者熟而中心喜說其進自不能已矣.\n程子曰習重習也時復思繹浹洽於中則說也又曰學者將以行之也時習之則所學者在我故悅.\n謝氏曰時習者無時而不習坐如尸坐時習也立如齊立時習也'\n有朋自遠方來不亦樂乎\n'朋同類也自遠方來則近者可知.程子曰以善及人而信從者衆故可樂又曰說在心樂主發散在外'\n人不知而不慍不亦君子乎\n'慍含怒意君子成德之名.\n尹氏曰學在己知不知在人何慍之有.\n程子曰雖樂於及人不見是而無悶乃所謂君子.\n愚謂及人而樂者順而易不知而不慍者逆而難故惟成德者能之然德之所以成亦由學之正習之熟說之深而不已焉耳'.\n有子曰其爲人也孝弟而好犯上者鮮矣不好犯上而好作亂者未之有也\n'有子孔子弟子名若善事父母爲孝善事兄長爲弟犯上謂干犯在上之人鮮少也作亂則爲悖逆爭鬪之事矣此言人能孝弟則其心和順少好犯上必不好作亂也'.\n君子務本本立而道生孝弟也者其爲仁之本與\n'務專力也本猶根也仁者愛之理心之德也爲仁猶曰行仁與者疑辭謙退不敢質言也言君子凡事專用力於根本根本旣立則其道自生若上文所謂孝弟乃是爲仁之本學者務此則仁道自此而生也.\n程子曰孝弟順德也故不好犯上豈復有逆理亂常之事德有本本立則其道充大孝弟行於家而後仁愛及於物所謂親親而仁民也故爲仁以孝弟爲本論性則以仁爲孝弟之本或問孝弟爲仁之本此是由孝弟可以至仁否曰非也謂行仁自孝弟始孝弟는 是仁之一事니 謂之行仁之本則可謂是仁之本則不可蓋仁是性也孝弟是用也性中只有箇仁義禮智四者而已曷嘗有孝弟來然仁主於愛愛莫大於愛親故曰孝弟也者其爲仁之本與'.\n子曰巧言令色鮮矣仁"
+}
+console.log(document.querySelector('#book-content').value )
 
-if (qs.text) {
-    var smalltext = qs.text.split("'")
+var text = document.querySelector('#book-content').value.replace(/\./g, '<br>')
+var title = document.querySelector('#book-title').value.replace(' ', '<br></br>')
+
+renderText(title, text)
+
+function renderText(title, text) {
+
+    var smalltext = text.split("'")
     for (var i=0; i<smalltext.length; i++){
         if (i%2 == 1){
             texttonumarr[i] = []
-            smalltextarr[i] = smalltext[i].match(/.{1,4}/g)
+            smalltextarr[i] = smalltext[i].replace(/\s/g, '').replace(/\<br\>/g, '○').match(/.{1,4}/g)
             for (var j=0; j<smalltextarr[i].length; j++){
                 texttonumarr[i][j] = base64[j]
             }
@@ -36,10 +45,8 @@ if (qs.text) {
     }
     var smalltextstring = smalltext.join('')
     
-    text = smalltextstring.replace(/\s/g, '').replace(/\\n/g, '○')
-}
+    text = smalltextstring.replace(/\s/g, '').replace(/\<br\>/g, '○')
 
-if (text) {
     var text2 = text.match(/.{1,12}/g)
     var index = -1
     var k = 0
@@ -76,8 +83,29 @@ if (text) {
             }
         }
     }
-}
 
-if (title){
     document.querySelector('#booktitle').innerHTML = title
 }
+
+
+document.querySelector('#book-title').addEventListener("input", (e) => {
+    title = document.querySelector('#book-title').value
+    renderText(title, text)
+})
+
+document.querySelector('#book-content').addEventListener("input", (e) => {
+    text = document.querySelector('#book-content').value.replace(/\./g, '<br>')
+    renderText(title, text)
+})
+
+var isFolded = true
+
+document.querySelector('#outerline').addEventListener("click", (e) => {
+    if (isFolded) {
+        document.querySelector('.form').style.display = "flex"
+        isFolded = false
+    } else {
+        document.querySelector('.form').style.display = "none"
+        isFolded = true
+    }
+})
